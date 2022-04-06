4f1eb7b4dab8e7cc268a4a6c3bcdadb28e8536bf
James Eagle <jimeagle@gmail.com>
2022-04-06 22:16:49 +0100

    Save last addr

--- main.go
+++ main.go
@@ -17,11 +17,43 @@ const maxGoroutines = 10
 
 func main() {
 
+	// Get where we left off
+	f, err := os.OpenFile("start.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	savedAddress, err := netip.ParseAddr(string(b))
+	if err != nil {
+		log.Println(err)
+	}
+
+	guard := make(chan struct{}, maxGoroutines)
+	
 	prefix, err := netip.ParsePrefix("0.0.0.0/0")
 	if err != nil {
-		fmt.Printf("Failed to parse prefix: %v", err)
-		return
+		log.Fatal(err)
 	}
+
+	var latest netip.Addr
+
+	defer func() {
+		_, err = f.WriteString(latest.String())
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	mcnet.DefaultDialer = mcnet.Dialer{Dialer: &net.Dialer{}}
 
@@ -35,6 +67,10 @@ func main() {
 			continue
 		}
 
+		if savedAddress.Compare(addr) == 1 {
+			continue
+		}
+
 		wg.Add(1)
 		guard <- struct{}{}
 		go func(addr netip.Addr) {
@@ -43,6 +79,9 @@ func main() {
 				bar.Increment()
 				wg.Done()
 				<-guard
+				if addr.Compare(latest) == 0 {
+					latest = addr
+				}
 			}()
 
 			resp, delay, err := bot.PingAndList(fmt.Sprintf("%s:%d", addr.String(), 25565))
