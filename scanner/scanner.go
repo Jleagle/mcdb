dd1e5a0723ca2e14b38e81d9cab1d8077feaa89e
James Eagle <jimeagle@gmail.com>
2026-05-01 21:20:14 +0100

    Update logs

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"log"
 	"time"
+
+	"github.com/Jleagle/mcdb/storage"
 )
 
 type Storage interface {
-	SaveServer(s Server) error
-	GetOldestServer() (Server, error)
+	SaveServer(s storage.Server) error
+	GetOldestServer() (storage.Server, error)
 }
 
 func Updater(store Storage) {
@@ -25,19 +27,24 @@ func Updater(store Storage) {
 		cancel()
 
 		if err != nil {
-			log.Printf("Background updater: failed to probe %s: %v", server.IP, err)
+			log.Printf("\033[31mBackground updater: failed to probe %s: %v\033[0m", server.IP, err)
 			// Mark as updated anyway so we don't loop on it
 			server.IsOnline = false
-			store.SaveServer(server)
+			if err := store.SaveServer(server); err != nil {
+				log.Printf("Background updater: failed to save %s: %v", server.IP, err)
+			}
 		} else {
-			store.SaveServer(*status)
+			if err := store.SaveServer(*status); err != nil {
+				log.Printf("Background updater: failed to save %s: %v", server.IP, err)
+			}
+
 			if status.IsOnline {
-				log.Printf("Background updater: updated %s (Online)", server.IP)
+				log.Printf("\033[32mBackground updater: updated %s (Online)\033[0m", server.IP)
 			} else {
-				log.Printf("Background updater: updated %s (Offline)", server.IP)
+				log.Printf("\033[31mBackground updater: updated %s (Offline)\033[0m", server.IP)
 			}
 		}
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
